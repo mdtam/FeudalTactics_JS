2eadb41156a0aeba9fe900f382afea96d0b399f8
tamtam <tameremad.me@gmail.com>
2024-04-28 11:57:23 +0200

    add the hex map example and test it

--- feudal-tactics-js/src/game/main.ts
+++ feudal-tactics-js/src/game/main.ts
@@ -1,9 +1,9 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from "./scenes/Boot";
+import { GameOver } from "./scenes/GameOver";
+import { Game as MainGame } from "./scenes/Game";
+import { MainMenu } from "./scenes/MainMenu";
+import { AUTO, Game } from "phaser";
+import { Preloader } from "./scenes/Preloader";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -11,21 +11,16 @@ const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
     width: 1024,
     height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    parent: "game-container",
+    backgroundColor: "#2d2d2d",
+    // backgroundColor: "#028af8",
+    pixelArt: true,
+    scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 const StartGame = (parent: string) => {
-
     return new Game({ ...config, parent });
-
-}
+};
 
 export default StartGame;
+
--- feudal-tactics-js/src/game/scenes/Game.ts
+++ feudal-tactics-js/src/game/scenes/Game.ts
@@ -1,36 +1,55 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
+export class Game extends Scene {
+    controls: Phaser.Cameras.Controls.SmoothedKeyControl;
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
 
-    constructor ()
-    {
-        super('Game');
+    constructor() {
+        super("Game");
     }
 
-    create ()
-    {
+    create() {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
 
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        // const map = this.add.tilemap("map");
+        // const tileset = map.addTilesetImage("tileset", "tiles")!;
 
-        EventBus.emit('current-scene-ready', this);
+        // map.createLayer("Calque 1", tileset);
+
+        // const cursors = this.input.keyboard.createCursorKeys();
+        // const controlConfig = {
+        //     camera: this.cameras.main,
+        //     left: cursors.left,
+        //     right: cursors.right,
+        //     up: cursors.up,
+        //     down: cursors.down,
+        //     acceleration: 0.02,
+        //     drag: 0.0005,
+        //     maxSpeed: 0.7,
+        // };
+
+        // this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(
+        //     controlConfig
+        // );
+
+        // this.cameras.main.setZoom(2);
+        // this.cameras.main.centerOn(200, 100);
+
+        EventBus.emit("current-scene-ready", this);
     }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
+    changeScene() {
+        this.scene.start("GameOver");
     }
+
+    // update(_time: unknown, delta: number) {
+    //     this.controls.update(delta);
+    // }
 }
+
