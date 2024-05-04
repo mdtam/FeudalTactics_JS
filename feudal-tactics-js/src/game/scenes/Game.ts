721e28ca198f636870c5c0682e5d476adb452582
tamtam <tameremad.me@gmail.com>
2024-05-04 12:30:13 +0200

    fix map, add big layer and paint it later. add zoom

--- feudal-tactics-js/src/game/scenes/Game.ts
+++ feudal-tactics-js/src/game/scenes/Game.ts
@@ -13,29 +13,45 @@ export class Game extends Scene {
 
     create() {
         this.camera = this.cameras.main;
-        this.camera.setZoom(2);
-        this.camera.centerOn(200, 100);
-        // this.camera.setBackgroundColor(0x00ff00);
-        // this.background = this.add.image(512, 384, "background");
-        // this.background.setAlpha(0.5);
+        this.camera.setZoom(0.5);
+        this.camera.centerOn(50, 50);
 
         this.add.text(400, 300, "hey what's up!!");
 
         const map = this.add.tilemap("map");
-        const tileset = map.addTilesetImage("tileset", "tiles")!;
+        const tileset = map.addTilesetImage("tileset", "tiles");
+        map.createLayer("island", tileset);
+        // const graphics = this.add.graphics({ x: 0, y: 0 });
+        // map.renderDebugFull(graphics);
 
-        map.createLayer("Calque 1", tileset);
+        const mapWidth = 5;
+        const mapHeight = 5;
 
-        const cursors = this.input.keyboard!.createCursorKeys(); // asserted
+        for (let row = 0; row < mapHeight; row++) {
+            for (let col = 0; col < mapWidth; col++) {
+                map.putTileAt(Phaser.Math.RND.integerInRange(1, 13), col, row);
+            }
+        }
+        console.log("JSON: MAP");
+        console.log(map);
+
+        const cursors = this.input.keyboard.createCursorKeys();
         const controlConfig = {
             camera: this.camera,
             left: cursors.left,
             right: cursors.right,
             up: cursors.up,
             down: cursors.down,
-            acceleration: 0.7,
-            drag: 0.005,
-            maxSpeed: 0.7,
+            zoomIn: this.input.keyboard.addKey(
+                Phaser.Input.Keyboard.KeyCodes.NUMPAD_SUBTRACT
+            ),
+            zoomOut: this.input.keyboard.addKey(
+                Phaser.Input.Keyboard.KeyCodes.NUMPAD_ADD
+            ),
+            zoomSpeed: 0.008,
+            acceleration: 1,
+            drag: 0.05,
+            maxSpeed: 0.5,
         };
 
         this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(
@@ -51,6 +67,8 @@ export class Game extends Scene {
 
     update(_time: unknown, delta: number) {
         this.controls.update(delta);
+        this.controls.maxSpeedX = this.controls.maxSpeedX / this.camera.zoom;
+        this.controls.maxSpeedY = this.controls.maxSpeedY / this.camera.zoom;
     }
 }
 
