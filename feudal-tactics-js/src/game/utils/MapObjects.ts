dc529365fea635d601fedbf8ed04f0ae4997492a
Sami Emad <samiemad.me@gmail.com>
2024-07-06 16:49:21 +0200

    Translate all MapObjects

---
+++ feudal-tactics-js/src/game/utils/MapObjects.ts
@@ -0,0 +1,67 @@
+type MapObject ={
+    spriteName:string;
+    strength: number;
+};
+
+const Capital = {
+    spriteName: "capital",
+    strength: 1,
+} as const;
+
+const Castle = {
+    spriteName: "castle",
+    strength: 2,
+    cost: 15,
+} as const;
+
+const Gravestone = {
+    spriteName: "gravestone",
+    strength: 0,
+} as const;
+
+const Tree = {
+    spriteName: "tree",
+    strength: 0,
+} as const;
+
+const PalmTree = {
+    spriteName: "palm_tree",
+    strength: 0,
+} as const;
+
+const Peasant = {
+    spriteName: "peasant",
+    strength: 1,
+    salary: 2,
+    cost: 10,
+} as const;
+
+const Spearman = {
+    spriteName: "spearman",
+    strength: 2,
+    salary: 6,
+    cost: 10,
+} as const;
+
+const Knight = {
+    spriteName: "knight",
+    strength: 3,
+    salary: 18,
+    cost: 10,
+} as const;
+
+const Baron = {
+    spriteName: "baron",
+    strength: 4,
+    salary: 54,
+    cost: 10,
+} as const;
+
+const Units = [Peasant, Spearman, Knight, Baron] as const;
+
+type UnitType = typeof Units[number];
+
+type Unit = {
+    canAct: boolean;
+    unitType: UnitType;
+}
